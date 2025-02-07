c89035be53a1a0cd35b155eecdadd1c0bdbaea2d
«Ura23» <ura238723@gmail.com>
2025-02-07 15:19:22 +0300

    2

--- conf/settings.py
+++ conf/settings.py
@@ -48,7 +48,9 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            BASE_DIR, 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,7 +111,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Используйте STATICFILES_DIRS для списка директорий
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Преобразуйте BASE_DIR в строку
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- cook/views.py
+++ cook/views.py
@@ -4,9 +4,21 @@ from .models import Category, Post
 
 def index(request):
     posts = Post.objects.all()
-    
+    categories = Category.objects.all()
     context = {
         "title": "Главная страница",
-        "posts": posts
+        "posts": posts,
+        "categories": categories
+    }
+    return render (request, 'cook/index.html', context)
+
+
+def category_list(request, pk):
+    posts = Post.objects.filter(category_id=pk)
+    categories = Category.objects.all()
+    context = {
+        "title": posts[0].category,
+        "posts": posts,
+        "categories": categories
     }
     return render (request, 'cook/index.html', context)
